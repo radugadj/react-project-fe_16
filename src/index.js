d1a119b83be62df33dc673b64ba043890bfea54d
radugadj <radugadj@gmail.com>
2022-03-04 01:17:25 +0300

    Rework

--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-=======
-import { BrowserRouter } from 'react-router-dom';
->>>>>>> origin/developer
 
 import App from './App';
 
 import './styles/index.scss';
 
 
-
 ReactDOM.render(
-<<<<<<< HEAD
         <Router>
             <App />
         </Router>,  
-=======
-    
-    <BrowserRouter>
-    <App />
-</BrowserRouter>,  
-
->>>>>>> origin/developer
 document.getElementById('root'));
 
